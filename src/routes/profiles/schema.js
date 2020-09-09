19a5153475c9380451f9886f6ce6523211c6bfe3
Emmanuel Adedeji <emmans4destiny@gmail.com>
2020-09-09 19:26:04 +0300

    Update schema.js

--- src/routes/profiles/schema.js
+++ src/routes/profiles/schema.js
@@ -1,5 +1,4 @@
 const { model, Schema } = require("mongoose");
-const bcrypt = require("bcryptjs");
 
 const profileSchema = new Schema(
   {
@@ -11,23 +10,19 @@ const profileSchema = new Schema(
     email: {
       type: String,
       required: true,
-      // validate: {
-      //   validator: async function (email) {
-      //     const user = await profileModel.find({ email: email });
-      //     console.log(user);
-      //     if (user.length > 0) {
-      //       let error = new Error();
-      //       error.message = "THIS EMAIL ALREADY EXISTS";
-      //       throw error;
-      //     }else return true
-      //   },
-      //   message: "User already exists!",
-      // },
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 7,
+      validate: {
+        validator: async function (email) {
+          const user = await profileModel.find({ email: email });
+          console.log(user);
+          if (user.length > 0) {
+            let error = new Error();
+            error.message = "THIS EMAIL ALREADY EXISTS";
+            throw error;
+          }else return true
+        },
+        message: "User already exists!",
+      },
+     
     }, //VALIDATION
     bio: { type: String, required: true },
     title: { type: String, required: true },
@@ -36,57 +31,33 @@ const profileSchema = new Schema(
     username: {
       type: String,
       required: true,
-      // validate: {
-      //   validator: async function (username) {
-      //     const user = await profileModel.find({ username: username });
-      //     if (user.length > 0) {
-      //       let error = new Error();
-      //       error.message = "THIS  USERNAME already EXISTS";
-      //       throw error;
-      //     }else return true
-      //   },
-      //   message: "User already exists!",
-      // },
+      validate: {
+        validator: async function (username) {
+          const user = await profileModel.find({ username: username });
+          if (user.length > 0) {
+            let error = new Error();
+            error.message = "THIS  USERNAME already EXISTS";
+            throw error;
+          }else return true
+        },
+        message: "User already exists!",
+      },
     }, //validaton
-    facebookId: String
+ 
   },
+  
   { timestamps: true }
 );
-profileSchema.statics.findByCredentials = async (email, password) => {
-  const user = await profileModel.findOne({ email }) ;
-  const isMatch = await bcrypt.compare(password, user.password) ;
-  if (!isMatch) {
-    const err = new Error("Unable to login") ;
-    err.httpStatusCode = 401 ;
-    throw err;
-  }
-  return user ;
-} ;
 
-profileSchema.pre("save", async function (next) {
+profileSchema.methods.toJSON = function () {
   const user = this
-  if (user.isModified("password")) {
-    user.password = await bcrypt.hash(user.password, 8) ;
-  }
-  next() ;
-}) ;
-profileSchema.post("validate", function (error, doc, next) {
-  if (error) {
-    error.httpStatusCode = 400 ;
-    next(error) ;
-  } else {
-    next() ;
-  }
-}) ;
+  const userObject = user.toObject()
 
-profileSchema.post("save", function (error, doc, next) {
-  if (error.name === "MongoError" && error.code === 11000) {
-    error.httpStatusCode = 400 ;
-    next(error) ;
-  } else {
-    next() ;
-  }
-}) ;
-const profileModel = model("profiles", profileSchema);
+  delete userObject.password
+  delete userObject.__v
+
+  return userObject
+}
+const profileModel = model("profile", profileSchema);
 
 module.exports = profileModel;
